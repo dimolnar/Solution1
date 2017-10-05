32c94bfccd6afe0445b1ca4a479332f2ce5e2b6c
Dijana Molnar <dijanamolnar89@gmail.com>
2017-10-05 21:29:31 +0200

    Dodali smo VB.net projekt

---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Zdravo narode");
+            System.Console.ReadKey(true);
+        }
+    }
+}
